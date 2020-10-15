774edbb7f4ab62fd48c1355b21a9a67c73123f2c
LukaszCiesielski-PL <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2020-10-15 04:11:17 +0200

    Upload

---
+++ Rational/RationalConsoleAppDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RationalLib;
+namespace RationalConsoleAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Rational32.HALF);
+            Rational32 u = new Rational32(1, 2);
+            var w = new Rational32(0, 0);
+            var z = new Rational32();
+            Rational32 x = u;
+
+            Console.WriteLine(u);
+            Console.WriteLine(w);
+            Console.WriteLine(z);
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Rational/RationalLib/Rational32.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RationalLib
+{
+    public readonly struct Rational32
+    {
+        private readonly int numerator;
+        public int Numerator => numerator;
+
+        private readonly int denominator;
+        public int Denominator => denominator;
+
+        public static Rational32 ZERO;
+        public static Rational32 ONE;
+        public static Rational32 HALF;
+
+        static Rational32()
+        {
+            ZERO = new Rational32(0, 1);
+            ONE = new Rational32(1, 1);
+            HALF = new Rational32(1, 2);
+        }
+
+        public Rational32(int numerator, int denominator = 1)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public override string ToString() => $"{Numerator}/{Denominator}";
+        
+    }
+}
